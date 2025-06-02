58c4a062ba6b64a10da60b9419b3a4e08983b688
pol2007O <opaul8538@gmail.com>
2025-06-01 23:35:26 -0500

    Add files via upload

---
+++ Funcionamiento.js
@@ -0,0 +1,16 @@
+ function marcaincorrecta(){
+    document.getElementById('palabra').style="display: block;"
+    document.getElementById('bien').style="display: block;"
+}
+
+function Cambio(){
+    document.getElementById('palabra').style="display: none;"
+    document.getElementById('palabra2').style="display: block;"
+}
+
+function Cambio2(){
+     document.getElementById('palabra3').style="display: none;"
+     document.getElementById('palabra4').style="display: block;"
+}
+
+
